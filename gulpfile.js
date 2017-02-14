10ccaf1d534948961abf88a7dbd8658ae1c782f1
Tomas Trescak <tomi.trescak@gmail.com>
2017-02-14 15:11:10 +1100

    Automatic hashing and bundle split in production

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,16 @@ const runSequence = require('run-sequence');
 const spawn = require('child_process').spawn;
 const child_process = require('child_process');
 const fsbx = require("fuse-box");
+const hash_src = require("gulp-hash-src");
+const htmlmin = require("gulp-htmlmin");
+
 let node;
 /**
  * ********************************************************
  * REACT UI build
  */
 const devReactFolder = 'dist/client/development';
+const prodReactFolder = 'dist/client/production';
 
 /**
  * Project dependencies
@@ -23,6 +27,11 @@ const REACT_DEPS = `
 gulp.task("copy-ui-development-html", () => {
     gulp.src("src/client/index.html")
         .pipe(gulp.dest(devReactFolder))
+});
+
+gulp.task("copy-ui-production-html", () => {
+    gulp.src("src/client/index.html")
+        .pipe(gulp.dest(prodReactFolder))
 });
 
 const ReactDevelopment = (opts) => {
@@ -66,6 +75,15 @@ gulp.task("run-react-ui", ['copy-ui-development-html'], () => {
 gulp.task("dist-react-production", (done) => {
     fsbx.FuseBox.init({
         homeDir: "src/client",
+        log: false,
+        outFile: `${prodReactFolder}/bundles/vendor.js`,
+        plugins: [
+            fsbx.UglifyJSPlugin()
+        ]
+    }).bundle(REACT_DEPS);
+
+    fsbx.FuseBox.init({
+        homeDir: "src/client",
         log: true,
         cache: false,
         globals: { default: "MySuperApplication" },
@@ -81,8 +99,24 @@ gulp.task("dist-react-production", (done) => {
 
             fsbx.UglifyJSPlugin()
         ],
-        outFile: `dist/client/production/bundle.min.js`,
+        outFile: `${prodReactFolder}/bundles/app.js`,
     }).bundle(">application.tsx", done);
+});
+
+gulp.task("hash", function() {
+    console.log(prodReactFolder)
+
+    gulp.src("src/client/index.html")
+        .pipe(gulp.dest(prodReactFolder))
+        .pipe(htmlmin())
+        .pipe(hash_src({verbose: true, build_dir: `./${prodReactFolder}`, src_path: `./${prodReactFolder}`}))
+        .pipe(gulp.dest(`./${prodReactFolder}`))
+});
+
+gulp.task("hash2", function() {
+    return gulp.src([`./${prodReactFolder}/index.html`])
+        .pipe(hash_src({ verbose: true, build_dir: `./${prodReactFolder}`, src_path: `./${prodReactFolder}`}))
+        .pipe(gulp.dest(`./${prodReactFolder}`))
 });
 
 
@@ -97,9 +131,6 @@ gulp.task("fuse-box-server", (done) => {
         outFile: "dist/server/development/app.js",
     }).bundle(">[app.ts]", done);
 });
-
-
-
 
 gulp.task('server', function() {
     if (node) node.kill()
@@ -127,6 +158,6 @@ gulp.task('dev', ['copy-ui-development-html'], function() {
 });
 
 
-gulp.task("dist", ['dist-react-production'], () => {
-
+gulp.task("dist", () => {
+    runSequence('dist-react-production', 'hash')
 });
